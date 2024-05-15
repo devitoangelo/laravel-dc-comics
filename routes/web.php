6b8428fc34ca987e93e09e9baed6fd68a46c7f3a
devitoangelo <angelodevito4@gmail.com>
2024-05-15 16:56:40 +0200

    add resource route
    Add the resorce route for comics

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ComicController;
+use App\Models\Comic;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ComicController::class, 'index'])->name('home');
+
+
+Route::resource('/comics', ComicController::class);
